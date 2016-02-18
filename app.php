b8ab6a8bcd0f42496b450e1cdc7577ad7532dfbe
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-18 10:56:33 +0200

    added php

--- app.php
+++ app.php
@@ -1,6 +1,15 @@
-asdsad
-adsasddsa
-asdsa ads
-sadsadasddsa
-asdsda
-asddsa
+<?php
+?>
+<h2> First application </h2>
+
+<form method="get">
+	<label for ="message">Message *<label> </br>
+	<input>
+	
+	<input name="message"></br>
+	<input type="text" name ="message"></br>
+	<input type="submit" value ="Save to DB">
+
+
+
+<form>
