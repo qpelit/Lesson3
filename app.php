4d6a7cee0763fab0885bebebd35fba172f4ee274
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-18 12:43:16 +0200

    added to and from and to if there are

--- app.php
+++ app.php
@@ -1,7 +1,55 @@
 <?php
+	//check if there is variable in the URL
+		//**********************
+	//TO validation
+	//**********************
+		if(isset($_GET["to"])){
+		
+	if(empty($_GET["to"])){
+		echo "Please enter TO"."<br>";
+		}
+		else {
+			echo "TO: =".$_GET["to"]."<br>";
+		}
+	}
+	else{
+		
+	}
+	//**********************
+	//From validation
+	//**********************
+	if(isset($_GET["from"])){
+		
+	if(empty($_GET["from"])){
+		echo "Please enter From"."<br>";
+		}
+		else {
+			echo "from: =".$_GET["from"]."<br>";
+		}
+	}
+	else{
+		
+	}
+	//**********************
+	//message validation
+	//**********************	
+	if(isset($_GET["message"])){
+		
+	if(empty($_GET["message"])){
+		echo "Please enter a message"."<br>";
+		}
+		else {
+			echo "Message: =".$_GET["message"]."<br>";
+		}
+	}
+	else{
+		
+	}
 
 // Getting the message from adddress
 //if there is ?name=.. then $_GET["message"]
+
+/*
 $my_message = $_GET["message"];
 $my_to = $_GET["to"];
 $my_from = $_GET["from"];
@@ -10,6 +58,7 @@ echo('<br>');
 echo "From : ".$my_from;
 echo('<br>');
 echo " Message :".$my_message;
+*/
 ?>
 <h2> First application </h2>
 
