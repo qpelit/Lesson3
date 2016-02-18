f9015fd1e71030f6ebcc1111a2c8e4737a523852
qpelit <qpelit@greeny.cs.tlu.ee>
2016-02-18 11:22:21 +0200

    added to and message

--- app.php
+++ app.php
@@ -1,13 +1,20 @@
 <?php
+
+// Getting the message from adddress
+//if there is ?name=.. then $_GET["message"]
+$my_message = $_GET["message"];
+$my_to = $_GET["to"];
+echo "To : ".$my_to;
+echo('<br>');
+echo " Message :".$my_message;
 ?>
 <h2> First application </h2>
 
 <form method="get">
-	<label for ="message">Message *<label> </br>
-	<input>
-	
-	<input name="message"></br>
-	<input type="text" name ="message"></br>
+<label for ="to">TO	</label> 
+<input type="text" name ="to"><br><br>
+	<label for ="message">Message *</label>
+	<input type="text" name ="message"><br><br>
 	<input type="submit" value ="Save to DB">
 
 
